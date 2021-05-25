80cf79b1e4505a650d78e88570a3a73ec8902fc5
Bruno Silver <bmbelo87@gmail.com>
2021-05-25 19:20:30 -0300

    Primeiro Commit

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace CAD.Series
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ Enum/Genero.cs
@@ -0,0 +1,19 @@
+namespace CAD.Series
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Espionagem = 6,
+        Faroeste = 7,
+        Gantasia = 8,
+        Ficcao_Cientifica = 9,
+        Musical = 10,
+        romance = 11,
+        Suspense = 12,
+        Terror = 13
+    }
+}
